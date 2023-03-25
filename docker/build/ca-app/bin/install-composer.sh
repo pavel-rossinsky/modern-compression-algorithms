39ca7891469b8a464f4fdd83de1baaad26cc435f
Pavel Rossinsky <po.rossinsky@gmail.com>
2023-03-25 12:33:06 +0100

    Install composer

--- docker/build/ca-app/bin/install-composer.sh
+++ docker/build/ca-app/bin/install-composer.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install whatever relies upon docker env variables in this script.
-
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 chmod +x /usr/local/bin/composer
